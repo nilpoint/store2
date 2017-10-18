0d83ba4673de3a9e042e62e2e6ac077c3ca24b54
John Smith <nilpoint@hotmail.com>
2017-10-18 19:48:33 +0800

    Declare two text elements and initialize them

--- app/src/main/java/cn/topca/store2/StoreActivity.java
+++ app/src/main/java/cn/topca/store2/StoreActivity.java
@@ -2,9 +2,17 @@ package cn.topca.store2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class StoreActivity extends AppCompatActivity {
+    // 1.Declare some ui elements
+    private EditText mUIKeyEdit, mUIValueEdit;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -15,5 +23,9 @@ public class StoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+        // 2.Initializes text components.
+        mUIKeyEdit = (EditText) findViewById(R.id.uiKeyEdit);
+        mUIValueEdit = (EditText) findViewById(R.id.uiValueEdit);
     }
 }
