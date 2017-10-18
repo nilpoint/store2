75588f7625d4b79b5188a3abc4eb3ef744cb339b
John Smith <nilpoint@hotmail.com>
2017-10-18 20:48:51 +0800

    Explore store c interface to java

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,51 @@
-#include <jni.h>
-#include <string>
+#include <cstdlib>
+#include "Store.h"
 
 extern "C" {
+/**
+ * Contains the unique store instance in a static variable created
+ * when library is loaded.
+ */
+static Store gStore;
+
+JNIEXPORT jint JNI_OnLoad(JavaVM* pVM, void* reserved) {
+    // Store initialization.
+    gStore.mLength = 0;
+    return JNI_VERSION_1_6;
+}
+
+JNIEXPORT jint JNICALL Java_cn_topca_store2_StoreActivity_getCount
+        (JNIEnv* pEnv, jobject pObject) {
+    return gStore.mLength;
+}
+
+JNIEXPORT jstring JNICALL Java_cn_topca_store2_StoreActivity_getString
+        (JNIEnv* pEnv, jobject pThis, jstring pKey) {
+    StoreEntry* entry = findEntry(pEnv, &gStore, pKey);
+    if (isEntryValid(pEnv, entry, StoreType_String)) {
+        // Converts a C string into a Java String.
+        return pEnv->NewStringUTF(entry->mValue.mString);
+    } else {
+        return NULL;
+    }
+}
+
+JNIEXPORT void JNICALL Java_cn_topca_store2_StoreActivity_setString
+        (JNIEnv* pEnv, jobject pThis, jstring pKey, jstring pString) {
+    // Turns the Java string into a temporary C string.
+    StoreEntry* entry = allocateEntry(pEnv, &gStore, pKey);
+    if (entry != NULL) {
+        entry->mType = StoreType_String;
+        // Copy the temporary C string into its dynamically allocated
+        // final location. Then releases the temporary string.
+        jsize stringLength = pEnv->GetStringUTFLength(pString);
+        entry->mValue.mString = new char[stringLength + 1];
+        // Directly copies the Java String into our new C buffer.
+        pEnv->GetStringUTFRegion(pString, 0, stringLength,
+                                 entry->mValue.mString);
+        // Append the null character for string termination.
+        entry->mValue.mString[stringLength] = '\0';
+    }
+}
 
 }
